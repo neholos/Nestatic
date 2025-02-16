a63e40bb9541fe1a228d5489c1fbe69448889847
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-02-17 00:28:02 +0200

    Add basic search

--- Nestatic/ViewController.swift
+++ Nestatic/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import GiphyUISDK
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    private let bundle = Bundle.main
+    
+    private lazy var giphyAPIKey = bundle.object(forInfoDictionaryKey: "GIPHYAPIKey") as! String
+    
+    private var searchTask: Task<Void, Never>?
+    
+    private lazy var searchController: UISearchController = {
+        let controller = UISearchController()
+        controller.searchResultsUpdater = self
+        return controller
+    }()
+    
+    private lazy var gridController: GiphyGridController = {
+        let controller = GiphyGridController()
+        controller.setAPIKey(giphyAPIKey)
+        controller.cellPadding = 2.0
+        controller.direction = .vertical
+        controller.numberOfTracks = 3
+        controller.fixedSizeCells = true
+        return controller
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationItem.searchController?.searchBar.becomeFirstResponder()
+    }
+    
+    private func setupUI() {
+        navigationItem.searchController = searchController
+        view.backgroundColor = .systemBackground
+        
+        addChild(gridController)
+        view.addSubview(gridController.view)
+        
+        gridController.view.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
 
+        gridController.didMove(toParent: self)
+    }
 }
 
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text, !query.isEmpty else { return }
+        
+        searchTask?.cancel()
+        searchTask = Task {
+            try? await Task.sleep(nanoseconds: 500_000_000)
+            gridController.content = GPHContent.search(withQuery: query, mediaType: .gif, language: .english)
+            gridController.update()
+        }
+    }
+}
